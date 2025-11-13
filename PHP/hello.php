7dbbb16ee26c62b6f125ddfa2c7fd55fe78d1e7b
kaomatsu8888 <m.kaoru0830@gmail.com>
2025-11-13 23:19:09 +0900

    form

---
+++ PHP/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<!-- ここは頭の部分 -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<!-- ここは本文の部分 -->
+<body>
+    Let's greet the world!<br>
+    <?php
+    print "Hello, World!\n";
+    ?>
+</body>
+</html>
